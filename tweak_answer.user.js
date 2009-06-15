bf314b3c28d7125662a7dbd6a3092e1632a8c214
Alexey Ten (Lynn) <alexeyten+deb@gmail.com>
2009-06-16 01:17:52 +0400

    Добавлена возможность всегда ответить действием
    Closes #6

--- tweak_answer.user.js
+++ tweak_answer.user.js
@@ -93,43 +93,40 @@ var the_script = function() {
         }
     } // }}}
 
-    var add_congratulation = function(r) { // {{{
+    var add_menuitems = function(r) { // {{{
         var oclk = function(a) { return (function() {return a;}) }
         var s = y5.Dom.getDescendant(r.control, 'div', 'b-actions-select')
         var links = s.getElementsByTagName('a')
+        var new_items = {
+            slashme: { title: 'ответить действием', data: [ 'slashme', '' ] },
+            congratulation: { title: 'поздравить!', data: [ 'congratulation', { id: r.commonParams.author_id, login: r.commonParams.author_login, title: r.commonParams.author_title }, '', '', '' ] }
+        }
+
         for (var i = 2; i < links.length; i++) {
             if (!links[i].onclick) return
             var l_par = links[i].onclick()
+            if (new_items[l_par[0]]) {
+                delete(new_items[l_par[0]])
+            }
             if (l_par[0] == 'congratulation') {
-                if (l_par[3] == 'birthday') {
-                    return
+                if (l_par[3] != 'birthday') {
+                    l_par[3] = l_par[4]
+                    l_par[4] = ''
+                    links[i].onclick = oclk(l_par)
                 }
-                l_par[3] = l_par[4]
-                l_par[4] = ''
-                var a = document.createElement('a')
-                a.href = links[i].href
-                a.innerHTML = links[i].innerHTML
-                a.onclick = oclk(l_par)
-                links[i].parentNode.appendChild(a)
-                links[i].parentNode.removeChild(links[i])
-                return
             }
         }
+
         var h = links[0].parentNode.parentNode
-        var d = document.createElement('div')
-        var a = document.createElement('a')
-        a.href = "#"
-        a.innerHTML = 'поздравить!'
-        d.appendChild(a)
-        h.insertBefore(d, null)
-        var _par = [
-            'congratulation',
-            {   id:    r.commonParams.author_id,
-                login: r.commonParams.author_login,
-                title: r.commonParams.author_title },
-            '', '', ''
-        ]
-        a.onclick = oclk(_par)
+        for (var i in new_items) {
+            var d = document.createElement('div')
+            var a = document.createElement('a')
+            a.href = "#"
+            a.innerHTML = new_items[i].title
+            a.onclick = oclk(new_items[i].data)
+            d.appendChild(a)
+            h.insertBefore(d, null)
+        }
     } // }}}
 
     var patch_ActionsControl = function(ac) { // {{{
@@ -142,7 +139,7 @@ var the_script = function() {
             form_preparer(this.form)
         }
         p.initActionsMenu = function(/* arguments */) {
-            add_congratulation(this)
+            add_menuitems(this)
             old_initActionsMenu.apply(this, arguments)
         }
     } // }}}
