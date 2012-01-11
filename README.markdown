421904edef135a4b641f13eb2449de1d5f91b71e
Alexey Ten (Lynn) <alexeyten@gmail.com>
2012-01-11 13:03:11 +0400

    Добавлена работа в Google Chrome и Opera

--- README.markdown
+++ README.markdown
@@ -1,21 +1,23 @@
 GreaseMonkey скрипты для Ярушки
 ===============================
 
-Как устанавливать и использовать эти скрипты подробно [написано](http://clubs.ya.ru/4611686018427409269/replies.xml?item_no=874)
-в [клубе Я.Рюшечки.](http://clubs.ya.ru/4611686018427409269/)
+Как устанавливать и использовать эти скрипты подробно [написано](http://clubs.ya.ru/shki/874)
+в [клубе Я.Рюшечки.](http://clubs.ya.ru/shki/)
 
 tweak_answer.user.js
 --------------------
 
-Последняя версия 2.0.0 [(установить)](http://github.com/alexeyten/yaru-tweaks/raw/2.0.0/tweak_answer.user.js)
+Последняя версия 2.0.1 [(установить)](http://github.com/alexeyten/yaru-tweaks/raw/2.0.1/tweak_answer.user.js)
 
 * можно написать заголовок при ответе «у себя»;
 * добавляет пункт «ответить действием» в список ответных действий;
 * по умолчанию ответные фотки не публикуются в своём дневнике.
 
-Работоспособность скрипта проверялась в Firefox 8.0.
+Работоспособность скрипта проверялась в Firefox 8.0, Chromium 16, Opera 11.60.
 
 Благодарности [Vessna](http://vessna005.ya.ru/)
+
+[Обсуждение](http://clubs.ya.ru/shki/4851) в клубе Я.Рюшечки.
 
 [История изменений](http://github.com/alexeyten/yaru-tweaks/raw/master/tweak_answer.changelog)
 
--- tweak_answer.user.js
+++ tweak_answer.user.js
@@ -1,74 +1,83 @@
 // ==UserScript==
-// @name           Tweak answer
-// @version        2.0.0
-// @namespace      http://gm.lynn.ru/
-// @description    Кастомизация формы ответа
-// @copyright      2009-2011, Alexey Ten (Lynn) (http://lynn.ru)
-// @include        http://*.ya.ru/*
-// @exclude        */update_session.xml*
+// @name        Tweak answer
+// @version     2.0.1
+// @namespace   http://gm.lynn.ru/
+// @description Кастомизация формы ответа
+// @copyright   2009-2012, Alexey Ten (Lynn) (http://lynn.ru)
+// @include     http://*.ya.ru/*
+// @exclude     */update_session.xml*
+// @match       http://*.ya.ru/*
 // ==/UserScript==
 
 (function() {
-  var w = unsafeWindow || window;
-  var $ = w.$;
-  var actions = w.ya.controls.actions;
+var the_script = function(ya, $) {
+    var actions = ya.controls.actions;
 
-  function replace_prepare(name) {
-    var old_prepare = actions[name].prepare;
-    var old_init = actions[name].init;
-    var old_buttons = actions[name].buttons;
-    actions[name].prepare = function(postData) {
-      console.log(arguments);
-      var res = old_prepare.apply(this, arguments) || {};
-      $.extend(res, {noTrackback: true, checkbox: true});
-      w.console.log('prepared ', name, res);
-      return res;
+    function replace_prepare(name) {
+        var old_prepare = actions[name].prepare;
+        var old_init = actions[name].init;
+        var old_buttons = actions[name].buttons;
+        actions[name].prepare = function(postData) {
+            var res = old_prepare.apply(this, arguments) || {};
+            $.extend(res, {noTrackback: true, checkbox: true});
+            return res;
+        }
+        if ('comment' == name) {
+            actions[name].init = function(postData) {
+                old_init.apply(this, arguments);
+                $(this).prepend('<input name="title" style="width:98%;display:none" disabled>');
+            }
+            actions[name].buttons = function($placeholder, postData) {
+                old_buttons.apply(this, arguments);
+                $placeholder.find('[name=trackback]').click(function() {
+                    var c = !!this.checked;
+                    var e = $('[name=title]', this.form);
+                    if (c) {
+                        e.removeAttr('disabled');
+                    } else {
+                        e.attr('disabled', '');
+                    }
+                    e.toggle(c);
+                });
+            }
+        }
     }
-    if ('comment' == name) {
-      actions[name].init = function(postData) {
-        old_init.apply(this, arguments);
-        $(this).prepend('<input name="title" style="width:98%;display:none" disabled>');
-      }
-      actions[name].buttons = function($placeholder, postData) {
-        old_buttons.apply(this, arguments);
-        $placeholder.find('[name=trackback]').click(function() {
-          var c = !!this.checked;
-          var e = $('[name=title]', this.form);
-          if (c) {
-            e.removeAttr('disabled');
-          } else {
-            e.attr('disabled', '');
-          }
-          e.toggle(c);
-        });
-      }
+    for (var k in {photo: true, comment: true, slashme: true}) {
+        replace_prepare(k);
     }
-  }
-  for (var k in {photo: true, comment: true, slashme: true}) {
-    replace_prepare(k);
-  }
 
-  var ac = w.ya.controls.ActionsControl;
-  var init = ac.prototype.init;
-  ac.prototype.init = function(o) {
-    var res = init.apply(this, [o]);
-    var enabled = {};
-    var actions = this.data.actions;
-    for (var i = 0; i < actions.length; i++) {
-      enabled[actions[i].action] = true;
+    var ac = ya.controls.ActionsControl;
+    var init = ac.prototype.init;
+    ac.prototype.init = function(o) {
+        var res = init.apply(this, [o]);
+        var enabled = {};
+        var actions = this.data.actions;
+        for (var i = 0; i < actions.length; i++) {
+            enabled[actions[i].action] = true;
+        }
+        if (!enabled.slashme) {
+            this.data.actions.push({title:'ответить действием', action: 'slashme'});
+        }
+        /*
+        if (!enabled.holiday) {
+            this.data.actions.push({title:'поздравить!', action: 'congratulation'});
+            this.data.holiday = {
+                id: 'с восстановлением рюшечек',
+                preposition: '', 
+                title: 'с восстановлением рюшечек'
+            }
+        }*/
+        return res;
     }
-    if (!enabled.slashme) {
-      this.data.actions.push({title:'ответить действием', action: 'slashme'});
-    }
-    /*
-    if (!enabled.holiday) {
-      this.data.actions.push({title:'поздравить!', action: 'congratulation'});
-      this.data.holiday = {
-        id: 'с восстановлением рюшечек',
-        preposition: '', 
-        title: 'с восстановлением рюшечек'
-      }
-    }*/
-    return res;
-  }
+}
+if (window.opera) {
+    the_script(ya, jQuery);
+} else {
+    var script = document.createElement('script');
+    script.type = 'application/javascript';
+    script.appendChild(document.createTextNode('(' + the_script.toString() + ')(ya, jQuery)'));
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
 })()
+
+// vim: et sw=4 ts=4
